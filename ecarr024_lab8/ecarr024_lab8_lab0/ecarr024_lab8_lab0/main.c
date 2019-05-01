14e91f53fa8d97de406eb813b43531f99e06b6be
Edward Carrasco <carrasco.edw@gmail.com>
2019-05-01 10:18:02 -0700

    Lab8 part1

---
+++ ecarr024_lab8/ecarr024_lab8_lab0/ecarr024_lab8_lab0/main.c
@@ -0,0 +1,223 @@
+/*
+ * ecarr024_lab8_lab0.c
+ *
+ * Created: 4/29/2019 4:19:04 PM
+ * Author : Edward-Laptop
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include "io.h"
+
+#define SET_BIT(p,i) ((p) |= (1 << (i)))
+#define CLR_BIT(p,i) ((p) &= ~(1 << (i)))
+#define GET_BIT(p,i) ((p) & (1 << (i)))
+
+/*-------------------------------------------------------------------------*/
+
+#define DATA_BUS PORTC		// port connected to pins 7-14 of LCD display
+#define CONTROL_BUS PORTD	// port connected to pins 4 and 6 of LCD disp.
+#define RS 6			// pin number of uC connected to pin 4 of LCD disp.
+#define E 7			// pin number of uC connected to pin 6 of LCD disp.
+
+/*-------------------------------------------------------------------------*/
+
+void LCD_ClearScreen(void) {
+	LCD_WriteCommand(0x01);
+}
+
+void LCD_init(void) {
+
+	//wait for 100 ms.
+	delay_ms(100);
+	LCD_WriteCommand(0x38);
+	LCD_WriteCommand(0x06);
+	LCD_WriteCommand(0x0f);
+	LCD_WriteCommand(0x01);
+	delay_ms(10);
+}
+
+void LCD_WriteCommand (unsigned char Command) {
+	CLR_BIT(CONTROL_BUS,RS);
+	DATA_BUS = Command;
+	SET_BIT(CONTROL_BUS,E);
+	asm("nop");
+	CLR_BIT(CONTROL_BUS,E);
+	delay_ms(2); // ClearScreen requires 1.52ms to execute
+}
+
+void LCD_WriteData(unsigned char Data) {
+	SET_BIT(CONTROL_BUS,RS);
+	DATA_BUS = Data;
+	SET_BIT(CONTROL_BUS,E);
+	asm("nop");
+	CLR_BIT(CONTROL_BUS,E);
+	delay_ms(1);
+}
+
+void LCD_DisplayString( unsigned char column, const unsigned char* string) {
+	LCD_ClearScreen();
+	unsigned char c = column;
+	while(*string) {
+		LCD_Cursor(c++);
+		LCD_WriteData(*string++);
+	}
+}
+
+void LCD_Cursor(unsigned char column) {
+	if ( column < 17 ) { // 16x1 LCD: column < 9
+		// 16x2 LCD: column < 17
+		LCD_WriteCommand(0x80 + column - 1);
+		} else {
+		LCD_WriteCommand(0xB8 + column - 9);	// 16x1 LCD: column - 1
+		// 16x2 LCD: column - 9
+	}
+}
+
+void delay_ms(int miliSec) //for 8 Mhz crystal
+
+{
+	int i,j;
+	for(i=0;i<miliSec;i++)
+	for(j=0;j<775;j++)
+	{
+		asm("nop");
+	}
+}
+
+/************************************************************************/
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B;// bit3 = 0: CTC mode (clear timer on compare)
+	// bit2bit1bit0=011: pre-scaler /64
+	// 00001011: 0x0B
+	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+	// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A = 125;    // Timer interrupt will be generated when TCNT1==OCR1A
+	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+	// So when TCNT1 register equals 125,
+	// 1 ms has passed. Thus, we compare to 125.
+	// AVR timer interrupt mask register
+	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1=0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+
+	//Enable global interrupts
+	SREG |= 0x80; // 0x80: 1000000
+}
+
+void TimerOff() {
+	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect) {
+	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
+		TimerISR(); // Call the ISR that the user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+/************************************************************************/
+/* ADC                                                                   */
+/************************************************************************/
+
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	// ADEN: setting this bit enables analog-to-digital conversion.
+	// ADSC: setting this bit starts the first conversion.
+	// ADATE: setting this bit enables auto-triggering. Since we are
+	//        in Free Running Mode, a new conversion will trigger whenever
+	//        the previous conversion completes.
+}
+
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF; // A is input
+	DDRB = 0xFF; PORTB = 0x00; // B is output
+	DDRC = 0xFF; PORTC = 0x00; // LCD data lines
+	DDRD = 0xFF; PORTD = 0x00; // D is output and LCD control lines
+	
+	unsigned short tempADC = 0x0000;
+	unsigned char tmpB = 0x00;
+	unsigned char tmpD = 0x00;
+	unsigned char number = 0;
+	unsigned short prevADC = tempADC;
+	
+	char lcdString[20];
+	char adpString[16];
+	char* prefix = "ADC ";
+	char* sufix = "  ";
+	
+	ADC_init();
+	LCD_init();
+	
+	TimerSet(500);
+	TimerOn();
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		tempADC = ADC;
+		
+		tmpB = (char) tempADC;
+		tmpD = (char) (tempADC >> 8);
+		
+		PORTB = tmpB;
+		PORTD = tmpD;
+		
+		LCD_ClearScreen();
+		LCD_Cursor(1);
+		if (tempADC > prevADC) {
+			sufix = " +";
+		} else if (tempADC < prevADC) {
+			sufix = " -";
+		} else {
+			sufix = "  ";
+		}
+		
+		strcpy(lcdString, ""); // clear string
+		
+		utoa(tempADC, adpString, 2);
+		strncat(lcdString, prefix, 4);
+		strncat(lcdString, adpString, 10);
+		strncat(lcdString, sufix, 2);
+		
+		LCD_DisplayString(1, lcdString);
+		
+		
+		prevADC = tempADC;
+		
+		// Timer
+		while (!TimerFlag);
+		TimerFlag = 0;
+    }
+}
+
